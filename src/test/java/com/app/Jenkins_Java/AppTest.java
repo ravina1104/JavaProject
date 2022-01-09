a44db9426c5c4800cde6adc28a90c5af1407d93d
ravina <ravinahazari11@gmail.com>
2022-01-09 10:03:03 +0530

    Added java demo application files&quot

---
+++ src/test/java/com/app/Jenkins_Java/AppTest.java
@@ -0,0 +1,20 @@
+package com.app.Jenkins_Java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/com/app/TestMain.java
@@ -0,0 +1,14 @@
+package com.app;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class TestMain {
+
+		@Test
+		public void testInputIsEven(){
+		assertTrue(Main.checkIfInputIsAnEvenNumber(22));
+		
+
+	
+	}
+
+}
